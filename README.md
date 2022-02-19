b8889d322813b67d8e61bdd17085d09cfbded3ca
Chris Langton <wsl@ZenBook>
2022-02-20 01:59:43 +1100

    remove deprecated cache

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PhaserioQuickStart
+
+
+![in game screenshot](.repo/ingame.jpg)
+
+
 Offline web app PhaserIO Quick Start with Webworker
+
+- Run `npm install`
+- start server with; `node server.js`
+- Open in browser http://localhost:8080/
--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,24 +1,25 @@
 var PhaserGame = {cacheReady: false};
 // Check if a new cache is available on page load.
-window.addEventListener('load', function(e) {
-  window.applicationCache.addEventListener('updateready', function(e) {
-    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-      window.applicationCache.update();
-      window.applicationCache.swapCache();
-    }
-  }, false);
-  window.applicationCache.addEventListener('noupdate', function(e){
-    PhaserGame.cacheReady = true;
-  }, false);
-  window.applicationCache.addEventListener('cached', function(e){
-    PhaserGame.cacheReady = true;
-  }, false);
-  window.applicationCache.addEventListener('error', function(e){
-    if (e.reason === 'manifest') {
-      PhaserGame.cacheReady = true;
-    }
-  }, false);
-}, false);
+// window.addEventListener('load', function(e) {
+//   window.applicationCache.addEventListener('updateready', function(e) {
+//     if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+//       window.applicationCache.update();
+//       window.applicationCache.swapCache();
+//     }
+//   }, false);
+//   window.applicationCache.addEventListener('noupdate', function(e){
+//     PhaserGame.cacheReady = true;
+//   }, false);
+//   window.applicationCache.addEventListener('cached', function(e){
+//     PhaserGame.cacheReady = true;
+//   }, false);
+//   window.applicationCache.addEventListener('error', function(e){
+//     if (e.reason === 'manifest') {
+//       PhaserGame.cacheReady = true;
+//     }
+//   }, false);
+// }, false);
+
 document.addEventListener("DOMContentLoaded", function(e) {
   PhaserGame.game = new Phaser.Game(746, 420, Phaser.AUTO, '');
   PhaserGame.game.state.add('Boot', PhaserGame.Boot);
@@ -26,18 +27,19 @@ document.addEventListener("DOMContentLoaded", function(e) {
   PhaserGame.game.state.add('MainMenu', PhaserGame.MainMenu);
   PhaserGame.game.state.add('Game', PhaserGame.Game);
   PhaserGame.game.state.start('Boot');
+  PhaserGame.cacheReady = true;
 }, false);
 
 // if (typeof Worker != 'undefined') {
 //     var worker = new Worker('/assets/js/Worker.js');
 //     worker.onerror = function(e) {
-//         //document.getElementById('log').insertAdjacentHTML('beforeEnd','error encountered'+"\n"); 
+//         //document.getElementById('log').insertAdjacentHTML('beforeEnd','error encountered'+"\n");
 //         error('Ops, something bad occurred!', e);
 //     };
 //     worker.onmessage = function(e) {
-//         //document.getElementById('log').insertAdjacentHTML('beforeEnd', JSON.stringify(e.data)+"\n" ); 
+//         //document.getElementById('log').insertAdjacentHTML('beforeEnd', JSON.stringify(e.data)+"\n" );
 //         debug(e.data);
 //     };
-    
+
 //     worker.postMessage({start:true});
 // }
